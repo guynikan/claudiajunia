0f5c9afcd855768653b9bb2b8cd3890bc6e6a48c
guynikan <jr-lol@live.com>
2023-02-22 21:24:34 -0300

    comented service menu option and add nuxtlink instead of a element

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -87,7 +87,7 @@ const recentPosts = [
         <PopoverGroup as="nav" class="hidden space-x-10 md:flex">
           <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">Início</a>
 
-          <Popover class="relative" v-slot="{ open }">
+<!--           <Popover class="relative" v-slot="{ open }">
             <PopoverButton :class="[open ? 'text-gray-900' : 'text-gray-500', 'group inline-flex items-center rounded-md bg-white text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2']">
               <span>Serviços</span>
               <ChevronDownIcon :class="[open ? 'text-gray-600' : 'text-gray-400', 'ml-2 h-5 w-5 group-hover:text-gray-500']" aria-hidden="true" />
@@ -98,7 +98,7 @@ const recentPosts = [
                 <div class="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
                   <div class="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
                     <a v-for="item in services" :key="item.name" :href="item.href" class="-m-3 flex items-start rounded-lg p-3 hover:bg-gray-50">
-                      <!-- <component :is="item.icon" class="h-6 w-6 flex-shrink-0 text-indigo-600" aria-hidden="true" /> -->
+                      <component :is="item.icon" class="h-6 w-6 flex-shrink-0 text-indigo-600" aria-hidden="true" /> 
                       <div class="ml-4">
                         <p class="text-base font-medium text-gray-900">{{ item.name }}</p>
                         <p class="mt-1 text-sm text-gray-500">{{ item.description }}</p>
@@ -108,9 +108,9 @@ const recentPosts = [
                 </div>
               </PopoverPanel>
             </transition>
-          </Popover>
+          </Popover> -->
           <a href="#jobs" class="text-base font-medium text-gray-500 hover:text-gray-900">Jobs</a>
-          <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900">Sobre mim</a>
+          <NuxtLink to="/about" class="text-base font-medium text-gray-500 hover:text-gray-900">Sobre mim</NuxtLink>
 
         </PopoverGroup>
         <div class="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
